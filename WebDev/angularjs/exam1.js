4d9b90a2f3cabb33fc0f80984be866da30eec45e
Aadi <aadi.deshpande1@gmail.com>
2019-10-26 13:08:22 +0530

    javascript progs

---
+++ WebDev/angularjs/exam1.js
@@ -0,0 +1,13 @@
+//create a module
+var app1 = angular.module('app1', []);	
+//app1 is module name, [] are the dependencies
+
+//create a controller
+var mycontroller = function ($scope){
+	$scope.message = "angular js tut.";
+};
+//scope is the parameter object, message is the property
+
+
+//registering the controller to the module
+app1.controller("mycontroller", mycontroller);
